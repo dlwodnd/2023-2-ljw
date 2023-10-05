25a92a8e5a1f325f57a66ab4b49e3585b124bc2d
Greenstudy <leejaewoong300@gmail.com>
2023-10-05 16:21:36 +0900

    11

---
+++ src/main/java/com/green/day7/ch5/ArrayMissonResult.java
@@ -0,0 +1,12 @@
+ package com.green.day7.ch5;
+
+public class ArrayMissonResult {
+    public static void main(String[] args) {
+        int[] arr = {100, 200, 300, 400, 500};
+
+        //for 문과 arr 을 이용하여 배열의 모든 방의 값을 출력하시오
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
