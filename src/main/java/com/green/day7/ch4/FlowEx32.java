435cf1d13f7a8ce6bbdc3f9f891b9c19a8910742
Greenstudy <leejaewoong300@gmail.com>
2023-10-05 15:19:18 +0900

    11

--- src/main/java/com/green/day7/ch4/FlowEx32.java
+++ src/main/java/com/green/day7/ch4/FlowEx32.java
@@ -12,16 +12,19 @@ public class FlowEx32 {
         */
         Scanner scan = new Scanner(System.in);
         while (true) {
+            System.out.println("(1) square");
+            System.out.println("(2) tri");
+            System.out.println("(3) log");
             System.out.print("원하는 메뉴(1~3)를 선택하세요. (종료 = 0) >>");
             int num = scan.nextInt();
-            if (num > 0 && num < 4) {
-                System.out.printf("선택하신 메뉴는 %d번 입니다\n", num);
+            if (num < 0 || num > 3) {
+                System.out.println("메뉴를 잘못 입력하셨습니다.");
+                continue;
             } else if (num == 0) {
-                System.out.println("프로그램을 종료합니다");
                 break;
-            } else {
-                System.out.println("숫자를 잘못 입력하셨습니다.");
             }
+            System.out.printf("선택하신 메뉴는 %d번 입니다\n", num);
         }
+        System.out.println("프로그램을 종료합니다");
     }
 }
---
+++ src/main/java/com/green/day7/ch5/Array.java
@@ -0,0 +1,7 @@
+package com.green.day7.ch5;
+
+public class Array {
+    public static void main(String[] args){
+
+    }
+}
