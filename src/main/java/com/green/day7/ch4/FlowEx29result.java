f8f2772d83a0cb61e52fe98e70409e58bd1cad8e
Greenstudy <leejaewoong300@gmail.com>
2023-10-05 11:37:23 +0900

    11

--- src/main/java/com/green/day7/ch4/FlowEx29result.java
+++ src/main/java/com/green/day7/ch4/FlowEx29result.java
@@ -1,19 +1,18 @@
 package com.green.day7.ch4;
 
-public class FlowEx29 {
+public class FlowEx29result {
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
             System.out.printf("i = %d ", i);
             int temp = i;
-            while (temp > 0) {
-                int modVal = temp % 10;
-                if (modVal != 0 && modVal % 3 == 0) {
+
+            do {
+                if (temp % 10 % 3 == 0 && temp % 10 != 0) {
                     System.out.print("짝");
                 }
-
-                temp /= 10;
-            }
+            } while ((temp /= 10) != 0);
             System.out.println();
+
         }
     }
 }
