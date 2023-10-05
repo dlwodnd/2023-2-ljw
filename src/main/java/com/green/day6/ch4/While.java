3e69db2712842d03f898d21b876ac98c44d9a806
Greenstudy <leejaewoong300@gmail.com>
2023-10-05 09:49:02 +0900

    asdfg

--- src/main/java/com/green/day6/ch4/While.java
+++ src/main/java/com/green/day6/ch4/While.java
@@ -2,10 +2,12 @@ package com.green.day6.ch4;
 
 public class While {
     public static void main(String[] args) {
+        //for문은 몇번 돌려야 하는지 알떄
         for (int i = 1; i <= 10; i++) {
             System.out.println("for=" + i);
         }
         System.out.println("-----------------");
+        //while문은 언제 멈춰야 하는지 알때
         int i = 10;
         while (i >= 0) {
             System.out.println("while=" + i--);
