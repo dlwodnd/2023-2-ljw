5b3d9b03310a892b2aee9b784f02e8f2eb7e1f95
Greenstudy <leejaewoong300@gmail.com>
2023-10-10 16:30:15 +0900

    LJW20231010 1530

--- src/main/java/com/green/day9/ch5/ArrayEx18result.java
+++ src/main/java/com/green/day9/ch5/ArrayEx18result.java
@@ -1,6 +1,6 @@
 package com.green.day9.ch5;
 
-public class ArrayEx18 {
+public class ArrayEx18result {
     public static void main(String[] args) {
         int[][] score = {
                 {101, 102, 103}
@@ -9,9 +9,11 @@ public class ArrayEx18 {
                 , {41, 42, 43}
         };
         int sum = 0;
-        for (int i = 0 ; i < score.length ; i++){
-            for (int j = 0 ; j < score[i].length ; j++){
-                int x = score[i][j];
+        for (int i = 0; i < score.length; i++) {
+            int[] arr = score[i];
+            for (int j = 0; j < arr.length; j++) {
+                int x = arr[j];
+                System.out.printf("score[%d][%d] = %d\n", i, j, arr[j]);
                 sum += x;
             }
         }
