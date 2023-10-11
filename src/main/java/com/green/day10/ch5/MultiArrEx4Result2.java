d0784f2fca950d4e809d0bd06ec6d9a5be352b37
Greenstudy <leejaewoong300@gmail.com>
2023-10-11 11:59:03 +0900

    LJW20231011 1153

--- src/main/java/com/green/day10/ch5/MultiArrEx4Result2.java
+++ src/main/java/com/green/day10/ch5/MultiArrEx4Result2.java
@@ -2,25 +2,25 @@ package com.green.day10.ch5;
 
 import java.util.Scanner;
 
-public class MultiArrEx4Result {
+public class MultiArrEx4Result2 {
     public static void main(String[] args) {
-        String[][] word = {
+        String[][] words = {
                 {"chair", "의자"},
                 {"computer", "컴퓨터"},
                 {"integer", "정수"},
         };
         Scanner scan = new Scanner(System.in);
-        for (int i = 0; i < word.length; i++) {
-            int a = 1;
-            String[] wordArr = word[i];
-            System.out.printf("Q%d. %s의 뜻은?\n", i + 1, wordArr[0]);
+        int i = 1;
+        for (String[] wordArr : words) {
+            System.out.printf("Q%d. %s의 뜻은?\n", i++, wordArr[0]);
             String str = scan.nextLine();
-            if (!str.equals(wordArr[a])) {
-                System.out.printf("틀렸습니다. 정답은 %s입니다.\n", wordArr[a]);
-                i--;
+            if (!str.equals(wordArr[1])) {
+                System.out.printf("틀렸습니다. 정답은 %s입니다.\n", wordArr[1]);
                 continue;
             }
             System.out.println("정답입니다.");
+
+
         }
     }
 }
