b16e1e5893a9599c02d4a2cb981477c1ca055015
Greenstudy <leejaewoong300@gmail.com>
2023-10-16 18:10:23 +0900

    LJW20231016 1810

--- src/main/java/com/asdf/Test.java
+++ src/main/java/com/asdf/Test.java
@@ -4,17 +4,21 @@ import java.util.Arrays;
 
 public class Test {
     public static void main(String[] args) {
-        int[] sides = {3, 6, 5};
-        for (int i = 0; i < sides.length - 1; i++) {
-            for (int j = i + 1; j < sides.length; j++) {
-                if (sides[j] > sides[i]) {
-                    int max = sides[j];
-                    sides[j] = sides[i];
-                    sides[i] = max;
-                }
-            }
+        int hp = 999;
+        int answer = 0;
+        if(hp / 5 > 0){
+            answer += hp / 5;
+            hp = hp % 5;
+            System.out.printf("%d : %d\n",answer ,hp);
         }
-        System.out.println(Arrays.toString(sides));
+        if(hp / 3 > 0){
+            answer += hp / 3;
+            hp = hp % 3;
+            System.out.printf("%d : %d\n",answer ,hp);
+        }
+        answer += hp;
+        System.out.printf("%d : %d\n",answer ,hp);
+
 
 
     }
