622a9f50d5eb20cbabf114c1d060c8fc0af8490d
Greenstudy <leejaewoong300@gmail.com>
2023-10-17 15:25:00 +0900

    LJW20231017 1424

---
+++ src/main/java/com/green/day14/ch6/Car.java
@@ -0,0 +1,34 @@
+package com.green.day14.ch6;
+
+public class Car {
+    String color;
+    String gearType;//변속기 종류 - auto , manual
+    int door;//문 개수
+
+    public Car() {
+        this("white");
+    }
+
+    public Car(String color) {
+        this(color, "auto", 4);
+    }
+
+    public Car(String color, String gearType, int door) {
+        this.color = color;
+        this.gearType = gearType;
+        this.door = door;
+    }
+
+    public void printMySelf() {
+        System.out.printf("color = %s, gearType = %s, door = %d", color, gearType, door);
+    }
+}
+
+class CarTest {
+    public static void main(String[] args) {
+        Car car1 = new Car();
+        Car car2 = new Car("Blue");
+        car1.printMySelf();
+        car2.printMySelf();
+    }
+}
--- src/main/java/com/green/day14/ch6/CardTest.java
+++ src/main/java/com/green/day14/ch6/CardTest.java
@@ -6,10 +6,11 @@ public class CardTest {
         //void메소드(정확한 값도 없고 =이없기 때문이다)
         //Card c1 = cd.getCard();
 
-        for(int i = 0 ; i < 52 ; i++){
+        for(int i = 0 ; i < 10 ; i++){
             Card c = cd.getCard();
             c.printMySelf();
         }
+        cd.printAll();
 
 
     }
