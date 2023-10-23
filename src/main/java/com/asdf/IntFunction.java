d5569234236d9e8775a4f82b5b2322e6801b60d1
Greenstudy <leejaewoong300@gmail.com>
2023-10-23 19:29:52 +0900

    LJW202310123 1930

--- src/main/java/com/asdf/IntFunction.java
+++ src/main/java/com/asdf/IntFunction.java
@@ -1,4 +1,24 @@
 package com.asdf;
 
-public class IntFunction {
+public class IntFunction{
+    public static void main(String[] args) {
+        int[] x = {1,2,3,5,8,7,4,1,9,10};
+        int d = 0;
+        String str = "" ;
+        String str1 = "" ;
+        for (int i = 0; i < x.length; i++) {
+            if (x[i] % 2 == 0 ) {
+                str += x[i];
+            }else {
+                str1 += x[i];
+            }
+        }
+        System.out.println(str);
+        System.out.println(str1);
+        d = Integer.parseInt(str) + Integer.parseInt(str1);
+        System.out.println(d);
+
+    }
 }
+
+
