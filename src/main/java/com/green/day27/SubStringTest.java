e2ad70072955dddee45b5fee9d44aff8d7c999c1
Greenstudy <leejaewoong300@gmail.com>
2023-11-03 18:06:22 +0900

    LJW20231103 1806

---
+++ src/main/java/com/green/day27/SubStringTest.java
@@ -0,0 +1,10 @@
+package com.green.day27;
+
+public class SubStringTest {
+    public static void main(String[] args) {
+        String str = "ABCDEFQHIJK";
+        String r1 = str.substring(2,4);
+        System.out.println(r1);
+        System.out.println(str.substring(4));
+    }
+}
