ac8df74fc61dfd8727b0b8e5c81bbae051a3d19f
Greenstudy <leejaewoong300@gmail.com>
2023-11-06 18:30:02 +0900

    LJW20231106 1810

--- src/main/java/com/programmers/lessons120812/PgDangGu.java
+++ src/main/java/com/programmers/lessons120812/PgDangGu.java
@@ -14,24 +14,24 @@ class DangGu {
     public static int[] solution(int x, int y, int startX, int startY, int[][] balls) {
         int[] answer = new int[balls.length];
         for (int i = 0; i < balls.length; i++) {
-            int a = ((y - startY) + (y - balls[i][1])) * ((y - startY) + (y - balls[i][1]))+ (Math.abs(startX - balls[i][0]) * Math.abs(startX - balls[i][0]));
+            int a = (int)Math.pow((y - startY) + (y - balls[i][1]),2 )+(int)Math.pow(Math.abs(startX - balls[i][0]),2);
             int b = (startY + balls[i][1]) * (startY + balls[i][1]) + (Math.abs(startX - balls[i][0]) * Math.abs(startX - balls[i][0]));
-            int c = (startX + balls[i][0]) * (startX + balls[i][0]) + Math.abs(startY - balls[i][1]) * Math.abs(startY - balls[i][1]);
-            int d = ((x - startX) + (x - balls[i][0])) * ((x - startX) + (x - balls[i][0])) + (Math.abs(startY - balls[i][1]) * Math.abs(startY - balls[i][1]));
+            int c = (int)Math.pow((startX + balls[i][0]),2) +(int)Math.pow(Math.abs(startY - balls[i][1]),2);
+            int d = (int)Math.pow((x - startX) + (x - balls[i][0]),2)+ (int)Math.pow(Math.abs(startY - balls[i][1]),2);
             int[] news = {a, b, c, d};
 
             int min = Integer.MAX_VALUE;
             if (balls[i][1] == startY) {
                 if (y - startY > startY) {
-                    answer[i] = (startY * 2) * (startY * 2) + (startX - balls[i][0]) * (startX - balls[i][0]);
+                    answer[i] = (int)Math.pow(startY * 2,2) + (int)Math.pow(startX - balls[i][0],2);
                 } else {
-                    answer[i] = ((y - startY) * 2) * ((y - startY) * 2) + (startX - balls[i][0]) * (startX - balls[i][0]);
+                    answer[i] = (int)Math.pow((y - startY) * 2,2)+ (int)Math.pow((startX - balls[i][0]),2);
                 }
             } else if (balls[i][0] == startX) {
                 if (x - startX > startX) {
                     answer[i] = (int)Math.pow((startX * 2),2) + (int)Math.pow((startY - balls[i][1]),2);
                 } else {
-                    answer[i] = ((x - startX) * 2) * ((x - startX) * 2) + (startY - balls[i][1]) * (startY - balls[i][1]);
+                    answer[i] = (int)Math.pow((x - startX )* 2,2) + (int)Math.pow(startY - balls[i][1],2);
                 }
             } else if (balls[i][0] != startX && balls[i][1] != startY) {
                 for (int j = 0; j < news.length; j++) {
