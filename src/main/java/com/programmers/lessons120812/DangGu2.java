92f2f0c56a376e6986171b2be25a5e8245005ae7
Greenstudy <leejaewoong300@gmail.com>
2023-11-07 18:11:05 +0900

    LJW20231107 1800

--- src/main/java/com/programmers/lessons120812/DangGu2.java
+++
@@ -1,5 +0,0 @@
-package com.programmers.lessons120812;
-
-public class DangGu2 {
-
-}
---
+++ src/main/java/com/programmers/lessons120812/PgDangGu2.java
@@ -0,0 +1,18 @@
+package com.programmers.lessons120812;
+
+import java.util.Arrays;
+
+public class PgDangGu2 {
+    public static void main(String[] args) {
+        int[][] ex = {{5,8}};
+        int[] result = DangGu2.dangGu2(10, 10, 5, 9, ex);
+        System.out.println(Arrays.toString(result));
+    }
+}
+class DangGu2{
+    public static int[] dangGu2(int x,int y,int startX,int startY,int[][] balls){
+        int[] answer = new int[balls.length];
+
+        return answer;
+    }
+}
